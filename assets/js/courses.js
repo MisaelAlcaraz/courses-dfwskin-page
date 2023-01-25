5ae53ab43c3225e60322b7631802922a883c9a2b
antonioCrema <antonio@crema.cm>
2023-01-25 08:24:28 -0600

    main | Agregando detalles de cursos

---
+++ assets/js/courses.js
@@ -0,0 +1,153 @@
+ const courses = [
+    {
+        id: 1,
+        title: "Pestañas",
+        description:"La licencia en Pestañas, es una carrera para los amantes de las pestañas, te permitirá desarrollar habilidades en diferentes técnicas luciendo principalmente la mirada de una persona. Este programa está aprobado por el TDLR (Departamento de Licencias y Regulaciones de Texas) y se prepara al estudiante para cursar satisfactoriamente y el emprendimiento en nuevos negocios dentro de la industria de la belleza",
+        followers: 3,
+        hears: 4,
+        hours: 320,
+        img:"",
+        skills:[
+            'Colocación de pestañas postizas.',
+            'Levantamiento de pestañas.',
+            'Aconsejar sobre tamaños y cantidad de pestañas acorde al tipo de ojos.',
+            'Control higiénico de dicha actividad.',
+            'Utilizar la técnica adecuada para su colocación.'
+        ],
+        programs: [
+            'Conocimiento de todo el material a utilizar',
+            'Enfermedades y afecciones de la piel',
+            'Control de infecciones',
+            'Estudio de las diferentes formas del rostro',
+            'Diseños según tu forma de ojo  y rostro',
+            'Normas y reglamentaciones del estado y además te incluimos dos certificaciones adicionales',
+            'Pestañas de Volumen',
+            'Lifting & Tinte de pestañas'
+        ],
+        jobs: [
+            'Spas.',
+            'Hoteles.',
+            'Clínicas de Belleza.',
+            'Concursos de Belleza',
+            'Dentro de empresas de la industria cosmética.',
+            'Asesor de imagen en su propia consultoría privada.',
+            'Encargado de pasarelas y publicidad en el sector de modas.'
+        ]
+    },
+    {
+        id:  2,
+        title: "Cosmetologia",
+        description: "La cosmetología es la ciencia que permite obtener los conocimientos en colorimetría, corte y cuidado del cabello, uñas, cuidado y salud de la piel, productos el cuidado y salud de la piel, cabello, uñas, productos y técnicas para el embellecimiento.",
+        followers: 3,
+        hears: 4,
+        hours: 1000,
+        img: "",
+        programs: [
+            'Anatomía y estructura de la piel y el cabello.',
+            'Enfermedades de la piel, cuero cabelludo y uñas.',
+            'Análisis del cuero cabelludo y Control de infecciones.',
+            'Química y productos para el cuidado del cabello.',
+            'Procedimientos faciales básicos. Depilación de bello corporal y facial.',
+            'Maquillaje básico profesional.',
+            'Manicura.',
+            'Pedicure.',
+            'Alisado de cabello.',
+            'Ondulación permanente.',
+            'Extensiones de cabello.'
+        ],
+        skills:[
+            'Tratamientos faciales cosméticos.',
+            'Cosméticos para la piel del cuerpo.',
+            'Cosméticos para el cabello.',
+            'Cosmetología natural.',
+            'Tratamientos de relajación.',
+            'Cosméticos botánicos.',
+            'Estética holística o integral.',
+            'Maquillaje, etc.'
+        ],
+        jobs: [
+            'Spas.',
+            'Hoteles.',
+            'Clínicas de Belleza.',
+            'Concursos de Belleza',
+            'Dentro de empresas de la industria cosmética.',
+            'Asesor de imagen en su propia consultoría privada.',
+            'Encargado de pasarelas y publicidad en el sector de modas.'
+        ]
+    },
+    {
+        id: 3,
+        title: "Esteticista",
+        description:"Un esteticista es un profesional que tiene la capacidad de ofrecer tratamientos de belleza enfocados en la piel principalmente, de igual manera puede ofrecer servicios como faciales, corporales, cosméticos para manos y pies e incluso tratamientos especializados como es el caso de las terapias de rejuvenecimiento de piel.",
+        followers: 3,
+        hears: 4,
+        hours: 750,
+        img: "",
+        skills:[
+            'Limpieza de la piel.',
+            'Hidratación.',
+            'Masajes.',
+            'Drenaje linfático.',
+            'Aplicación cosmética.',
+            'Bronceado.',
+            'Depilación.',
+            'Armonización facial.'
+        ],
+        programs: [
+            'Anatomía y estructura de la piel. ',
+            'Enfermedades de la piel.',
+            'Análisis de la piel. ',
+            'Control de infecciones. ',
+            'Química & Productos para el cuidado de la piel. ',
+            'Procedimientos faciales.',
+            'Masajes faciales.',
+            'Aparatología facial.',
+            'Depilación de vello corporal & facial.',
+            'Maquillaje básico profesional.',
+            'Tratamientos avanzados (Peeling químicos, Derma-pen, Plasma -Pen).'
+        ],
+        jobs: [
+            'Salones de belleza.',
+            'Hoteles.',
+            'Spas.',
+            'Gimnasios.',
+            'Incluso desde casa.'
+        ]
+    },
+    {
+        id: 4,
+        title: "Manicura",
+        description: "Se llama manicuro,manicura o manicurista a la persona que tiene por ocupación el cuidado y embellecimiento de las manos y en especial, el cuidado y pintura de las uñas. sin contar con muchas otras ocupaciones propias del área.",
+        followers: 3,
+        hears: 4,
+        hours: 600,
+        img: "",
+        skills: [
+            'La aplicación de extensiones de uñas o de reparaciones.',
+            'La aplicación de técnicas artísticas en la uñas.',
+            'Recorte, perfilar y pulir las uñas.',
+            'Acondicionar la piel alrededor de las uñas con aceites y cremas.',
+            'Dar masajes en manos, pies o brazos para mejorar la circulación.',
+            'Tratamientos adaptados para el fortalecimiento de las uñas.',
+            'Tratamientos para que los clientes dejen de morderse las uñas.'
+        ],
+        programs: [
+           'Reglas y normas de TDLR',
+           'Control de infecciones ',
+           'Anatomía y fisiología de la piel',
+           'Trastornos y enfermedades de la piel ',
+           'Química de los productos para el cuidado de las uñas ',
+           'Procedimiento de manicura',
+           'Procedimiento de pedicura',
+           'Masajes de manos',
+           'Realce de uñas de gel',
+           'Realce de uñas acrílicas'
+        ],
+        jobs: [
+            'Salones de belleza.',
+            'Hoteles.',
+            'Spas.',
+            'Incluso desde casa.'
+        ]
+    }
+]
